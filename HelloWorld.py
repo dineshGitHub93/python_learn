5755c6972412252b169cfa4318e78a3f7036945b
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2024-11-26 10:02:48 +0530

    python variables

---
+++ HelloWorld.py
@@ -0,0 +1,1 @@
+print("Hello World, Welcome to python learning...!") #display text in the console
---
+++ Variables.py
@@ -0,0 +1,23 @@
+#Variables in python
+
+name = "Kunja" # i have stored string varibale 
+print(name)
+print ("Hi "+name)
+name = "Kunja kutty"
+print ("Hi "+name)
+
+#Interger or float varibles
+
+price = 45 #interger
+amount = 38.98 #floating point 
+
+#boolean variables
+#python is dynamically typed language
+isEmpty = True #boolean variable
+
+''' Multi-line comments 
+
+valid variables names :     name , first_name, _dollor, ball123
+invalid: name#, name$, 1name, reserved words cannot be accepted.
+
+'''
