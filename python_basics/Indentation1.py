25c9157e6a0996e98f0371e893b47cc163b4d8b1
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2025-01-03 21:44:02 +0530

    Indentation learning

---
+++ python_basics/Indentation1.py
@@ -0,0 +1,25 @@
+
+#If we Skip Indentation, Python will throw error
+if 10 > 5:
+    print("This is true block")
+    print("I am tab indentation")
+
+print("I have no indentation")
+
+
+#Indentation in Conditional Statements
+a = 20
+
+if a>= 18:
+    print(f"{a} is grater than 18 ")
+else:
+    print("Not Greater than")
+
+print("All set")
+
+#Indentation in Loops
+
+i = 5
+while i >= 1:
+    i -=1
+    print(i)
